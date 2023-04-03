c71dda2be95b227f10c393c8bd1ae312e939252c
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-04-02 23:44:45 -0300

    ♻️ Refactorr(gitignore):
    remove .env

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 import React from 'react';
 
 function Dashboard() {
@@ -5,3 +7,25 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  // console.log('getServersideProps');
+
+  const session = await getSession({ req });
+  console.log(session);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      props: {},
+    },
+  };
+};
