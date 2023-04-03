707be2b0f060706b5859bd5bc49224903bd0ca29
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-04-02 22:53:51 -0300

    ✨ feat(Login):
    adiciona funcionalidade de login com o google

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 function Header() {
+  const { data: session, status } = useSession();
   return (
     <header className="w-screen h-20 bg-slate-950 flex justify-center items-center">
       <section className="w-screen px-4 max-w-5xl flex items-center justify-between">
@@ -11,13 +13,32 @@ function Header() {
               Tarefas<span className="text-red-600">+</span>
             </h1>
           </Link>
-          <Link href="/dashboard" className="bg-slate-100 py-1 px-3 rounded-md">
-            Meu painel
-          </Link>
+          {session?.user && (
+            <Link
+              href="/dashboard"
+              className="bg-slate-100 py-1 px-3 rounded-md"
+            >
+              Meu painel
+            </Link>
+          )}
         </nav>
-        <button className="bg-transparent border-2 border-white rounded-full text-white px-5 py-1 font-semibold">
-          Acessar
-        </button>
+        {status === 'loading' ? (
+          <></>
+        ) : session ? (
+          <button
+            onClick={() => signOut()}
+            className="bg-transparent border-2 border-white rounded-full text-white px-5 py-1 font-semibold"
+          >
+            {`Olá ${session?.user?.name}`}
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn('google')}
+            className="bg-transparent border-2 border-white rounded-full text-white px-5 py-1 font-semibold"
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   );
